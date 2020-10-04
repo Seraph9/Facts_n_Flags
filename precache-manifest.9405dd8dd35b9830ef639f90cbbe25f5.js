8c56bedfb50f3aae9fea8eea77be5ff1600a369b
Abdullah Wafy <red.sulphur786@gmail.com>
2020-10-03 18:34:01 -0700

    Updates

---
+++ precache-manifest.9405dd8dd35b9830ef639f90cbbe25f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ba682a1cb67d70b395637648c77ef5",
+    "url": "/Facts_n_Flags/index.html"
+  },
+  {
+    "revision": "d024fcde9d01de3df916",
+    "url": "/Facts_n_Flags/static/css/main.22f477bb.chunk.css"
+  },
+  {
+    "revision": "1b24e5205458da51a178",
+    "url": "/Facts_n_Flags/static/js/2.0948a13c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Facts_n_Flags/static/js/2.0948a13c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d024fcde9d01de3df916",
+    "url": "/Facts_n_Flags/static/js/main.9b4092de.chunk.js"
+  },
+  {
+    "revision": "123c39791b2902c8cfe0",
+    "url": "/Facts_n_Flags/static/js/runtime-main.69ff0708.js"
+  }
+]);
