9cfa481a3c0154ad39c7c95be69ff9deb7171f87
Abdullah Wafy <red.sulphur786@gmail.com>
2020-10-16 22:44:39 -0700

    added react router navlink to navbar for logo name

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,8 @@ import { NavLink } from 'react-router-dom';
 function Navbar() {
     return (
         <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-            <a className='navbar-brand' href='/about'>FACTS 'N FLAGS</a>
+            {/* <a className='navbar-brand' href='/about'>FACTS 'N FLAGS</a> */}
+            <NavLink to="/about" className="navlinks" activeClassName="is-selected" exact={true}>FACTS 'N FLAGS</NavLink>
             <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNavAltMarkup' aria-controls='navbarNavAltMarkup' aria-expanded='false' aria-label='Toggle navigation'>
                 <span className='navbar-toggler-icon'></span>
             </button>
