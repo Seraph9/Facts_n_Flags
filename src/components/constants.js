72e159b534e04c70caedb55eee171aae6629959d
Abdullah Wafy <red.sulphur786@gmail.com>
2021-11-12 17:34:21 -0600

    API endpoint changed again in constants js file

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,5 +1,7 @@
 // const baseURL = "https://sampleapis.com/the-states/api/the-states"; // old REST API
 
-const baseURL = "https://api.sampleapis.com/the-states/the-states"; // new GRAPHQL API
+// const baseURL = "https://api.sampleapis.com/the-states/the-states"; // new GRAPHQL API
+
+const baseURL = "https://api.sampleapis.com/thestates/the-states"; // new modified GRAPHQL API
 
 export { baseURL }
